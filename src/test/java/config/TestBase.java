84a0c34fc32e8f686af029bb4fb2160796647019
Cosmin.Goanta <cosmingoanta1@gmail.com>
2023-06-27 16:38:08 +0300

    1st commit for automation tests on the-internet.herokuapp.com

--- src/test/java/config/TestBase.java
+++ src/test/java/config/TestBase.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -32,5 +33,9 @@ public class TestBase {
 
         driver = null;
         wait = null;
+    }
+
+    public WebDriver getDriver(){
+        return driver;
     }
 }
